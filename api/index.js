370d0067073e4fe228e489aa21034b5d0c3979db
uvindukaralliyadda <uvindukaralliyadda@gmail.com>
2024-12-21 11:53:04 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.Mongo).then( ()=>{
+    console.log("Connected to mongoDb");
+})
+.catch((err)=>{
+    console.log(err);
+});
 
 const app=express();
 
 app.listen(3000,()=>{
     console.log('server listening on port!');
 });
+
