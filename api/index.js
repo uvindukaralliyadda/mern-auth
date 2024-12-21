ee549ce953965eb4274ece5a9e7f0cf9fc236f21
uvindukaralliyadda <uvindukaralliyadda@gmail.com>
2024-12-21 15:07:27 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
+
+
 
 mongoose.connect(process.env.Mongo).then( ()=>{
     console.log("Connected to mongoDb");
@@ -13,6 +16,7 @@ mongoose.connect(process.env.Mongo).then( ()=>{
 const app=express();
 
 app.listen(3000,()=>{
-    console.log('server listening on port!');
+    console.log('server listening on port 3000');
 });
 
+app.use("/api/user", userRoutes);
